2696875557770ee7f4ee7bcbdf2a3fe866175898
Stacy <enjoyorhate@gmail.com>
2019-10-29 21:57:40 +0200

    Updates

---
+++ precache-manifest.279c9d35cf8ee8afd131bba2248178d4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8602f4c8ca7ea3b5bfbf9aa15281b5ee",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "278d7f7a60ff1fa4144e",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.078d5f9e.chunk.css"
+  },
+  {
+    "revision": "450c3c599517c67264cf",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.3e529801.chunk.js"
+  },
+  {
+    "revision": "278d7f7a60ff1fa4144e",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.a87e790f.chunk.js"
+  },
+  {
+    "revision": "a248bbf272ad9faaad52",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.e6999974.js"
+  },
+  {
+    "revision": "68bb8fab447d9f7e47f2dfcfd7dd14df",
+    "url": "/goit-react-hw-03-bank-account/static/media/down-arrow.68bb8fab.svg"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/goit-react-hw-03-bank-account/static/media/roboto-v20-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/goit-react-hw-03-bank-account/static/media/roboto-v20-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/goit-react-hw-03-bank-account/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/goit-react-hw-03-bank-account/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/goit-react-hw-03-bank-account/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/goit-react-hw-03-bank-account/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "c579b5949914f2578e5414ebce739a32",
+    "url": "/goit-react-hw-03-bank-account/static/media/up-arrow.c579b594.svg"
+  }
+]);
